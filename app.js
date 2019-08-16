d63542fdfeb13a3769662e0762c200d676dc675f
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-16 10:54:35 +0530

    server setting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Express=require('express');
 var app = new Express();
 app.set('view engine','ejs');
+app.use(Express.static(__dirname+"/public"));
 app.get('/',(rq,rs)=>{
     rs.render('index',{title:"MY website"});
 });
@@ -10,4 +11,4 @@ app.get('/login',(rq,rs)=>{
 app.get('/reg',(rq,rs)=>{
     rs.render('reg')
 });
-app.listen(3001);
+app.listen( process.env.PORT||3001);
